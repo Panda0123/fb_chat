635025132ebf26a93e79abc1fcc1077c57c163e0
Jamsed Cabili <jamsedcabili@gmail.com>
2020-02-16 13:56:57 +0800

    Added File

---
+++ fb_chat.py
@@ -0,0 +1,22 @@
+import fbchat
+from fbchat import Client
+from accounts import getPass
+from accounts import getUserName
+from accounts import getSendTo
+
+
+def main():
+    ps = getPass()
+    userName = getUserName()
+    client = fbchat.Client(userName, ps)
+
+    friends = client.searchForUsers(getSendTo())
+    friend = friends[0]
+    msg = "Wassup mamen"
+    response = client.send(fbchat.models.Message(msg), friend.uid)
+    if response:
+        print("Sended sucessfully")
+
+
+if __name__ == "__main__":
+    main()
